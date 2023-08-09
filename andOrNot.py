86f0c220b4d2c3261778cbf35e14070efe3b2253
Yusfar <106623966+yusufArslann@users.noreply.github.com>
2023-08-09 11:44:31 +0300

    Update andOrNot.py

--- andOrNot.py
+++ andOrNot.py
@@ -1,6 +1,16 @@
-durum = True 
+"""durum = True 
 
 if durum:
     print("Hazır!")
 else:
     print("Bir sorunla karşılaşıldı.")
+"""
+
+def sayi():
+    sayi1 = int(input("0'dan 10'a kadar bir sayı giriniz: "))
+    if sayi1 >=0 and sayi1 <=10:
+        print("Doğru sayı girdiniz.")
+    else:
+        print("0'dan 10'a kadar olan bir sayı girmeniz gerekiyor.")
+        sayi()
+sayi()
